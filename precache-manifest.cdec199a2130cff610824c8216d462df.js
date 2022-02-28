260c50a98318ce5260eecb34bb071f915b3aac04
Inuri-99 <96541@fhss.sjp.ac.lk>
2022-03-01 01:38:38 +0530

    Updates

---
+++ precache-manifest.cdec199a2130cff610824c8216d462df.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eac2fbc0a1a75073453ad7dd92099e3e",
+    "url": "/Inuri-99/Inuri-99.github.io.git/index.html"
+  },
+  {
+    "revision": "e6c2e85acf44dcf87bd6",
+    "url": "/Inuri-99/Inuri-99.github.io.git/static/css/2.14351861.chunk.css"
+  },
+  {
+    "revision": "361e79132732b5460fc9",
+    "url": "/Inuri-99/Inuri-99.github.io.git/static/css/main.4c8f5a95.chunk.css"
+  },
+  {
+    "revision": "e6c2e85acf44dcf87bd6",
+    "url": "/Inuri-99/Inuri-99.github.io.git/static/js/2.c452ac08.chunk.js"
+  },
+  {
+    "revision": "76c33606446526c6ec38bc107ec65a87",
+    "url": "/Inuri-99/Inuri-99.github.io.git/static/js/2.c452ac08.chunk.js.LICENSE"
+  },
+  {
+    "revision": "361e79132732b5460fc9",
+    "url": "/Inuri-99/Inuri-99.github.io.git/static/js/main.6665a6f5.chunk.js"
+  },
+  {
+    "revision": "5890672a01229fd94fd7",
+    "url": "/Inuri-99/Inuri-99.github.io.git/static/js/runtime-main.de5a99cc.js"
+  },
+  {
+    "revision": "e3c6aa57aa0ff7af0dfaaf8e3296829e",
+    "url": "/Inuri-99/Inuri-99.github.io.git/static/media/Inuri.e3c6aa57.jpg"
+  },
+  {
+    "revision": "38828042bce3def5dbc3c5b4a5d2df93",
+    "url": "/Inuri-99/Inuri-99.github.io.git/static/media/Pencil.38828042.jpg"
+  },
+  {
+    "revision": "b9c86d8dabf65498829505cbc9dce144",
+    "url": "/Inuri-99/Inuri-99.github.io.git/static/media/intro.b9c86d8d.jpg"
+  },
+  {
+    "revision": "87561258a0fb17472008a4b2ff66696f",
+    "url": "/Inuri-99/Inuri-99.github.io.git/static/media/laptop1.87561258.jpg"
+  }
+]);
